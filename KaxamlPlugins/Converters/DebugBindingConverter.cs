7fd25dd0545ecaa3303d17b5e8ce32b131f5d117
aesalazar <salazare@verizon.net>
2025-07-03 20:29:40 -0400

    base and debug binding converters

---
+++ KaxamlPlugins/Converters/DebugBindingConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace KaxamlPlugins.Converters;
+
+/// <summary>
+/// Allows for debugging bindings.
+/// Usage: Converter="{x:Static local:DebugBindingConverter.Instance}"
+/// </summary>
+public class DebugBindingConverter : BaseConverter<DebugBindingConverter>
+{
+    public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        Debug.WriteLine($"[DebugBindingConverter] Convert Value: {value}");
+        return value;
+    }
+
+    public override object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        Debug.WriteLine($"[DebugBindingConverter] ConvertBack Value: {value}");
+        return value;
+    }
+}
