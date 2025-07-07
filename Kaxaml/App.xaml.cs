f05cb5b087a09f8b70b10f0a26ddd6997512d094
aesalazar <salazare@verizon.net>
2025-07-07 11:49:42 -0400

    Setup microsoft logging service

--- Kaxaml/App.xaml.cs
+++ Kaxaml/App.xaml.cs
@@ -1,90 +1,98 @@
 using System;
-using System.Windows;
-using System.Data;
-using System.Xml;
-using System.Configuration;
-using System.Collections;
-using System.Text.RegularExpressions;
-using Kaxaml.Properties;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.ComponentModel;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
 using Kaxaml.Plugins.Default;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace Kaxaml
+namespace Kaxaml;
+
+public partial class App
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    private static string? _startupPath;
+    private IHost? _host;
 
-    public partial class App : Application
+    public Snippets? Snippets { get; set; }
+
+    public static string[] StartupArgs { get; private set; } = [];
+
+    public static string? StartupPath
     {
-        public App()
+        get
         {
-            InitializeComponent();
-        }
-
-        public Snippets? Snippets { get; set; }
-
-        private void app_Startup(object sender, StartupEventArgs e)
-        {
-            StartupArgs = e.Args;
-        }
-
-
-        [PreserveSig()]
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern int GetModuleFileName
-        (
-            [In]
-            HandleRef module,
-
-            [Out]
-            StringBuilder buffer,
-
-            [In]
-            [MarshalAs(UnmanagedType.U4)]int capacity
-        );
-
-        public static string[] StartupArgs { get; private set; } = Array.Empty<string>();
-
-        private static string? _startupPath;
-        public static string? StartupPath
-        {
-            get
+            // Only retrieve startup path when it wans’t known.
+            if (_startupPath == null)
             {
-                // Only retrieve startup path when
-                // it wans’t known.
-                if (_startupPath == null)
-                {
-                    var nullHandle =
-                        new HandleRef(null, IntPtr.Zero);
+                var nullHandle = new HandleRef(null, IntPtr.Zero);
+                var buffer = new StringBuilder(260);
+                var length = GetModuleFileName(
+                    nullHandle,
+                    buffer,
+                    buffer.Capacity);
 
-                    var buffer =
-                        new StringBuilder(260);
+                if (length == 0)
+                    // This ctor overload uses 
+                    // GetLastWin32Error to
+                    //get its error code.
+                    throw new Win32Exception();
 
-                    var lenght = GetModuleFileName(
-                        nullHandle,
-                        buffer,
-                        buffer.Capacity);
-
-                    if (lenght == 0)
-                    {
-                        // This ctor overload uses 
-                        // GetLastWin32Error to
-                        //get its error code.
-                        throw new Win32Exception();
-                    }
-
-                    var moduleFilename =
-                        buffer.ToString(0, lenght);
-                    _startupPath =
-                        Path.GetDirectoryName(moduleFilename);
-                }
-
-                return _startupPath;
+                var moduleFilename = buffer.ToString(0, length);
+                _startupPath = Path.GetDirectoryName(moduleFilename);
             }
+
+            return _startupPath;
         }
+    }
+
+    //TODO: See if we can use instead
+    //var x = System.Reflection.Assembly.GetExecutingAssembly().Location;
+    [PreserveSig]
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern int GetModuleFileName
+    (
+        [In] HandleRef module,
+        [Out] StringBuilder buffer,
+        [In] [MarshalAs(UnmanagedType.U4)] int capacity
+    );
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        StartupArgs = e.Args;
+
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+                logging.SetMinimumLevel(LogLevel.Information);
+            })
+            .ConfigureServices((_, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+                services.AddSingleton<App>();
+            })
+            .Build();
+
+        var logger = _host.Services.GetRequiredService<ILogger<App>>();
+        logger.LogInformation("Application is starting.");
+
+        var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (_host is not null)
+        {
+            _host.StopAsync().GetAwaiter().GetResult();
+            _host.Dispose();
+        }
+
+        base.OnExit(e);
     }
 }
