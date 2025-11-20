dc6c53a9a85960b3b5adc8bd7972ad330078c1c1
aesalazar <salazare@verizon.net>
2025-11-20 18:21:02 -0500

    add HideCloseButtonBehavior to SnippetEditor
    This forces the user to use either the Cancel or Done buttons
    which have side effects not the same as closing the window
    using the standard close button.

---
+++ KaxamlPlugins/Behaviors/HideCloseButtonBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using Microsoft.Xaml.Behaviors;
+
+namespace KaxamlPlugins.Behaviors;
+
+/// <summary>
+/// Hides the close button of a Window.
+/// </summary>
+/// <remarks>
+/// Cheap way to get out of overriding the Window Chrome.
+/// </remarks>
+public class HideCloseButtonBehavior : Behavior<Window>
+{
+    [DllImport("user32.dll")]
+    private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.Loaded += OnLoaded;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        AssociatedObject.Loaded -= OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        var hwnd = new WindowInteropHelper(AssociatedObject).Handle;
+        var style = GetWindowLong(hwnd, GWL_STYLE);
+        SetWindowLong(hwnd, GWL_STYLE, style & ~WS_SYSMENU);
+    }
+
+    // ReSharper disable InconsistentNaming
+    private const int GWL_STYLE = -16;
+
+    private const int WS_SYSMENU = 0x80000;
+    // ReSharper restore InconsistentNaming
+}
